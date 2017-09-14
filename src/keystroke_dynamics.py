f44b7a8c646d68b22c3a67184c3a84e8bd465b76
Vitor Pereira Scheffer <vitorps@windowslive.com>
2017-09-14 19:48:13 -0300

    novos ajustes

--- src/keystroke_dynamics.py
+++ src/keystroke_dynamics.py
@@ -41,7 +41,7 @@ def get_dados():
 
     return X, y, X_train, X_test, y_train, y_test, y_labels
 
-def learn_curve(estimator, title, X, y, ylim=None, cv=None,
+def plt_learn_curve(estimator, title, X, y, ylim=None, cv=None,
                         n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
     """
     Generate a simple plot of the test and training learning curve.
@@ -111,21 +111,18 @@ def learn_curve(estimator, title, X, y, ylim=None, cv=None,
 
 from matplotlib import pyplot as plt
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
+def plt_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix',cmap=plt.cm.Blues):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     """
-    if normalize:
+    """if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         print("Normalized confusion matrix")
     else:
         print('Confusion matrix, without normalization')
 
-    print(cm)
+    print(cm)"""
 
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
@@ -136,10 +133,10 @@ def plot_confusion_matrix(cm, classes,
 
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
-   # for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    # for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
     #    plt.text(j, i, format(cm[i, j], fmt),
-     #            horizontalalignment="center",
-      #           color="white" if cm[i, j] > thresh else "black")
+    #            horizontalalignment="center",
+    #           color="white" if cm[i, j] > thresh else "black")
 
     plt.tight_layout()
     plt.ylabel('True label')
